f70a27aeac4f6bbbfb42fe261b5a2f3d1d4a4cca
Eric Will <rakaur@malkier.net>
2011-09-27 17:11:16 -0400

    Fix a problem where Shrike had a #uid call instead of #key

--- lib/kythera/service/shrike.rb
+++ lib/kythera/service/shrike.rb
@@ -73,7 +73,7 @@ class ShrikeService < Service
 
         # Join our configuration channel
         $eventq.handle(:end_of_burst) do
-            @uplink.join(@user.uid, @config.channel) if @config.channel
+            @uplink.join(@user.key, @config.channel) if @config.channel
         end
     end
 
